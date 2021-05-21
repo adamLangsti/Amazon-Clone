5fbca6b05d4bfd760f9a1c1562a539d6ea6c76b8
Adam Lang <adam.lang-1@hotmail.com>
2021-05-21 18:44:01 +0200

    feat: header/navbar and home components in progress

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../images/amazon-logo-white.png';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src={logo} alt='amazon' />
+            <div className='header-search'>
+                <input className='header-search-input' />
+                <SearchIcon className='header-search-icon' />
+            </div>
+            <div className='header-nav'>
+                <div className='header-option'>
+                    <span className='header-optionLineOne'>Hello Guest</span>
+                    <span className='header-optionLineTwo'>Sign In</span>
+                </div>
+                <div className='header-option'>
+                    <span className='header-optionLineThree'>Returns</span>
+                    <span className='header-optionLineFour'>& Orders</span>
+                </div>
+                <div className='header-option'>
+                    <span className='header-optionLineFive'>Your</span>
+                    <span className='header-optionLineSix'>Prime</span>
+                </div>
+                <div className='header-optionBasket'>
+                    <ShoppingBasketIcon />
+                    <span className='header-basketCount'>0</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyA_JIdf23HIXAzMdmE49ukK_cHatR3HjGU',
+    authDomain: 'clone-8d348.firebaseapp.com',
+    projectId: 'clone-8d348',
+    storageBucket: 'clone-8d348.appspot.com',
+    messagingSenderId: '872128832896',
+    appId: '1:872128832896:web:0d2a529f1fec766ff10d65',
+};
