a5aef59d9a11b9339bc3306d7043b2c4f00ed5e0
Adam Lang <adam.lang-1@hotmail.com>
2021-05-26 11:22:19 +0200

    feat: basket shows correct amount with context api

--- src/components/SubTotal.js
+++ src/components/SubTotal.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from './context/Reducer';
+import { useStateValue } from './context/StateProvider';
 
 const SubTotal = () => {
+    const [{ basket }, dispatch] = useStateValue();
     return (
         <div className='subtotal'>
             <CurrencyFormat
                 renderText={(value) => (
                     <>
                         <p>
-                            Subtotal (0 items):
-                            <strong>0</strong>
+                            Subtotal ({basket.length} items):
+                            <strong>{value}</strong>
                         </p>
                         <small className='subtotal-gift'>
                             <input type='checkbox' />
@@ -18,7 +21,7 @@ const SubTotal = () => {
                     </>
                 )}
                 decimalScale={2}
-                value={0}
+                value={getBasketTotal(basket)}
                 displayType={'text'}
                 thousandSeparator={true}
                 prefix={'$'}
